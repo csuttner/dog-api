e46982fb3e7a6acfe7a1ab8c249a6f0b2af45272
Clay Suttner <csuttner@gmail.com>
2021-03-07 12:45:39 -0800

    adds serializers

---
+++ dogapi/dogs/serializers.py
@@ -0,0 +1,50 @@
+from rest_framework import serializers
+from games.models import GameCategory
+from games.models import Game
+from games.models import Player
+from games.models import PlayerScore
+
+class BreedSerializer(serializers.HyperlinkedModelSerializer):
+  dogs = serializers.HyperlinkedRelatedField(
+    many=True,
+    read_only=True,
+    view_name='dog-detail'
+  )
+  size = serializers.ChoiceField(choices=Breed.SIZE_CHOICES)
+  size_description = serializers.CharField(source='get_size_display', read_only=True)
+
+  class Meta:
+    model = Breed
+
+    fields = (
+      'url',
+      'pk',
+      'name',
+      'size',
+      'size_description',
+      'friendliness',
+      'trainability',
+      'sheddingamount',
+      'exerciseneeds',
+      'dogs'
+    )
+
+class DogSerializer(serializers.HyperlinkedModelSerializer):
+  breed = serializers.SlugRelatedField(queryset=Breed.objects.all(), slug_field='name')
+  gender = serializers.ChoiceField(choices=Dog.GENDER_CHOICES)
+  gender_description = serializers.CharField(source='get_gender_display', read_only=True)
+
+  class Meta:
+    model = Dog
+    
+    fields = (
+      'url',
+      'breed',
+      'name',
+      'age',
+      'gender',
+      'gender_description',
+      'color',
+      'favoritefood',
+      'favoritetoy'
+    )
